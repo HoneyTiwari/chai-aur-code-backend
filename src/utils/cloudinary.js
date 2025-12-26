d30bced53f6ec2b90c0ef549bd9553bc01ba150f
Honey Tiwari <honey.20240029@mnnit.ac.in>
2025-12-26 10:47:44 +0530

    initial commit

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,46 @@
+import {v2 as cloudinary} from "cloudinary";
+import fs from "fs";
+
+
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET,s
+});
+
+const uploadOnCloudinary = async (localFilePath)=>{
+  try {
+      if(!localFilePath) return null;
+      //upload file on cloudinary
+      cloudinary.uploader.upload(localFilePath,{
+        resource_type:"auto",
+        })
+        console.log("file is uploaded on cloudinary",response.url);
+        return response 
+  } catch (error) {
+    fs.unlinkSync(localFilePath)
+    //reemove the locally savede temporary file as the upload operation got failed
+    return null;
+  }
+}
+export {uploadOnCloudinary}
+
+
+
+
+
+
+
+
+
+
+
+
+
+cloudinary.v2.uploader
+.upload("dog.mp4", {
+  resource_type: "video", 
+  public_id: "my_dog",
+  overwrite: true, 
+  notification_url: "https://mysite.example.com/notify_endpoint"})
+.then(result=>console.log(result));
